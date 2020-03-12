617dc1f6881097852efa4bd9e934c0112524e83f
Troy-S <troysheed@gmail.com>
2020-03-12 20:51:11 +0000

    Added dev build eslint eslint-fix scripts to package.json & added dev and production config files.

---
+++ webpack/config.dev.babel.js
@@ -0,0 +1,75 @@
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    '.src/js/app.js',
+    '.src/scss/app.scss'
+  ],
+  output: {
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './src/html/',
+    watchContentBase: true,
+    open: true,
+    inline: true,
+    quiet: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new FriendlyErrorsWebpackPlugin({
+      clearConsole: true
+    }),
+    new HtmlWebpackPlugin({
+      template: '.src/html/index.html',
+      filename: './index.html'
+    })
+  ],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\/(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        enforce: 'pre',
+        test: /\/(js)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\/(css|scss)$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              url: false
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              indent: 'postcss',
+              sourceMap: true,
+              plugins: () => [
+                require('autoprefixer')()
+              ]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
