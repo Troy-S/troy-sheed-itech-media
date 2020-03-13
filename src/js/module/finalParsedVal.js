0ecc59c9b0f56cd33e54a652cd9eb6a2dba1f32a
Troy-S <troysheed@gmail.com>
2020-03-13 16:23:11 +0000

    imported isKeyType + parseOperations. Setup rest of conditionals needed.

--- src/js/module/finalParsedVal.js
+++ src/js/module/finalParsedVal.js
@@ -25,3 +25,64 @@
  * ---------------------------------------------------------------------------
  *
 */
+
+import isKeyType from './keyType';
+import parseOperations from './parseOperations';
+
+/**
+ * [This function returns what needs to be displayed by the calculator it return
+ *  the final parsed value]
+ * @param  {[String]}  key   [using key from isKeyType module]
+ * @param  {[Number]}  uiNum [Assigned default value for when a number is pressed]
+ * @param  {[Object]}  state [Initial state object and properties]
+ * @return {[Boolean]}       [Return what to do based on the typeOfKey pressed]
+ */
+const finalParsedValue = (key, uiNum, state) => {
+    /* Storing contents of a key */
+    const contents = key.textContent;
+    /* Assigning the type of key to a variable to use in out loigc later */
+    const typeOfKey = isKeyType(key);
+
+    /* Destructured State object which is going to handle our logic + UI State
+     *(in uiState.js)
+     */
+    const {
+        initialValue,
+        modifiedValue,
+        operation,
+        prevKeyType
+    } = state;
+
+    /* Checking the keys contents to see if it is clear, if true then return 0
+     * Testing to see if this function works in app.js when using
+     * addEventListener.
+     */
+    if (typeOfKey === 'clear') return 0;
+
+    /* If number key is pressed... */
+    if (typeOfKey === 'number') {
+        /* Do something */
+    }
+
+    /* If dot key is pressed... */
+    if (typeOfKey === 'dot') {
+        /* Do something */
+    }
+
+    /* If operation key is pressed... */
+    if (typeOfKey === 'operation') {
+        /* Do something */
+    }
+
+    /* If equals key is pressed... */
+    if (typeOfKey === 'equals') {
+        /* Do something */
+    }
+
+    /* If save key is pressed... */
+    if (typeOfKey === 'save') {
+        /* Do something */
+    }
+};
+
+export default finalParsedValue;
