4acd556ca03f233e701f39bfd77bd5a4b1129ccb
Troy-S <troysheed@gmail.com>
2020-03-13 19:32:05 +0000

    Imported all modules to main app.js + Have an event listener to initiate the project

--- src/js/app.js
+++ src/js/app.js
@@ -8,19 +8,20 @@
 
 import { myCalc, ui, keys } from './module/domSelectors';
 
-import parseOperations from './module/parseOperations';
-
 import isKeyType from './module/keyType';
+
+import parseOperations from './module/parseOperations';
 
 import finalParsedValue from './module/finalParsedVal';
 
-/* Testing the event listener works with dataset. Using this to initiate the
- * calculator.
- */
+import uiState from './module/uiState';
+
+/* Using this Event Listener to initiate calculator. */
 keys.addEventListener('click', (e) => {
     if (!e.target.matches('button')) return;
     const key = e.target;
     const uiNum = ui.textContent;
     const result = finalParsedValue(key, uiNum, myCalc.dataset);
     ui.textContent = result;
+    uiState(key, myCalc, result, uiNum);
 });
