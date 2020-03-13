a7b392866497489f4d06fa851ac2d13333167143
Troy-S <troysheed@gmail.com>
2020-03-13 14:35:51 +0000

    Added DOM Selectors and testing JSDoc (Will eventually replace pseudo code with JSDoc).

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,23 @@
  * https://github.com/airbnb/javascript
  *
 */
+
+/**
+ * [myCalc description]
+ * @type {[String]}
+ */
+const myCalc = document.querySelector('.calc');
+
+/**
+ * [display description]
+ * @type {[String]}
+ */
+const display = myCalc.querySelector('.calc__ui');
+
+/**
+ * [keys description]
+ * @type {[String]}
+ */
+const keys = myCalc.querySelector('.calc__numpad');
+
+console.log(keys, display, myCalc);
