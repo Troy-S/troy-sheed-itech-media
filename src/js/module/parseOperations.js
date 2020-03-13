54109feaa70b45a3cc23979f05ed00598ab15e23
Troy-S <troysheed@gmail.com>
2020-03-13 19:33:56 +0000

    Amended JsDoc for parseOperations

--- src/js/module/parseOperations.js
+++ src/js/module/parseOperations.js
@@ -16,11 +16,11 @@
 /**
  * [Calculating each possible operation. Using parseFloat as we want to be able
  *  to use a floating number in our calculator.]
- * @param  {[String]} digitOne  [First number pressed 0-9]
- * @param  {[String]} operation [Operation pressed +, -, *, ÷, clear, dot, equals]
- * @param  {[String]} digitTwo  [Second number pressed 0-9]
- * @return {[Boolean]}          [Checking if an operation equals one of these
- *                               If so then return the expression.]
+ * @param  {String}  digitOne  [First number pressed 0-9]
+ * @param  {String}  operation [Operation pressed +, -, *, ÷, clear, dot, equals]
+ * @param  {String}  digitTwo  [Second number pressed 0-9]
+ * @return {Boolean}           [Checking if an operation equals one of these
+ *                                If so then return the expression.]
  */
 const parseOperations = (digitOne, operation, digitTwo) => {
     /* Storing parseFloat */
