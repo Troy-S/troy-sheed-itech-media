55c2d3b7d836a521c4d7752fa7c8f61c7cef2287
Troy-S <troysheed@gmail.com>
2020-03-13 16:21:15 +0000

    Adding logic for isKeyType

--- src/js/module/keyType.js
+++ src/js/module/keyType.js
@@ -7,7 +7,36 @@
  * 4, 5, 6, 7, 8, 9].
  *
  * Then we will assign these keys to values and will return either a number,
- * operation or a task based on a conditional.
+ * operation or a id based on a conditional.
  * -----------------------------------------------------------------------------
  *
 */
+
+/**
+ * [This function is to determine which type of key was pressed and once the key
+ *  has been procesed we will assign it a value which will be either a number,
+ *  operation or it's neither of those then it should return an id.]
+ * @param  {[String]}  key [Assigning a key to a type.]
+ * @return {Boolean}       [Return a 'number' string, an 'operation' String
+ *                          or an 'id' string.]
+ */
+const isKeyType = (key) => {
+    /* Destructuring id from key.dataset */
+    const { id } = key.dataset;
+
+    /* If the key doesn't have a data-id then return a number */
+    if (!id) return 'number';
+
+    /* If the key is one of these id's then return an operation */
+    if (
+        id === 'multiply'
+        || id === 'divide'
+        || id === 'add'
+        || id === 'subtract'
+    ) return 'operation';
+
+    /* If it matches neither then return an id */
+    return id;
+};
+
+export default isKeyType;
