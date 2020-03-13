bc9ef4e8ba52ba14412f6911bfce98212b0544c6
Troy-S <troysheed@gmail.com>
2020-03-13 16:17:33 +0000

    updated and imported domSelectors, parseOperations, isKeyType and also testing they work with keys.addEventListener

--- src/js/app.js
+++ src/js/app.js
@@ -6,4 +6,21 @@
  *
 */
 
-import { myCalc, display, keys } from './module/domSelectors';
+import { myCalc, ui, keys } from './module/domSelectors';
+
+import parseOperations from './module/parseOperations';
+
+import isKeyType from './module/keyType';
+
+import finalParsedValue from './module/finalParsedVal';
+
+/* Testing the event listener works with dataset. Using this to initiate the
+ * calculator.
+ */
+keys.addEventListener('click', (e) => {
+    if (!e.target.matches('button')) return;
+    const key = e.target;
+    const uiNum = ui.textContent;
+    const result = finalParsedValue(key, uiNum, myCalc.dataset);
+    ui.textContent = result;
+});
